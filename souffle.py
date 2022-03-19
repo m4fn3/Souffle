cab40c03ab5ad65a9e0c3f7856d78c176d98dd7d
mafusuke <mafusukee@gmail.com>
2022-03-19 17:31:40 +0900

    参加/退出時メッセージ追加

--- souffle.py
+++ souffle.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 import pickle
 import time
 
+import response
+
 dev_guild = discord.Object(id=565434676877983772)
 
 
@@ -27,6 +29,20 @@ class Souffle(commands.Bot):
         # for e in self.get_guild(953185304267862066).emojis:
         #     print(f"{e.name} = \"<:{e.name}:{e.id}>\"")
 
+    async def on_guild_join(self, guild: discord.Guild) -> None:
+        """サーバー参加時"""
+        embed = discord.Embed(title=f"{guild.name} に参加しました。", color=0x00ffff)
+        embed.description = f"サーバーID: {guild.id}\nメンバー数: {len(guild.members)}\nサーバー管理者: {str(guild.owner)} ({guild.owner.id})"
+        await self.get_channel(744466739542360064).send(embed=embed)
 
-
-
+    async def on_guild_remove(self, guild: discord.Guild) -> None:
+        """サーバー退出時"""
+        embed = discord.Embed(title=f"{guild.name} を退出しました。", color=0xff1493)
+        embed.description = f"サーバーID: {guild.id}\nメンバー数: {len(guild.members)}\nサーバー管理者: {str(guild.owner)} ({guild.owner.id})"
+        await self.get_channel(744466739542360064).send(embed=embed)
+        if guild.id in self.verified_guilds:
+            self.verified_guilds.discard(guild.id)
+            channel = self.get_channel(888017049589260298)
+            await channel.send(embed=response.success(f"{guild.name}({guild.id})を退出したので自動的に承認を取り下げました."))
+            with open('guilds.pickle', 'wb') as f:
+                pickle.dump(self.verified_guilds, f)
